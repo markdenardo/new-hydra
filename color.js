3af84dccd72ae6dc09c093ad19ac3ae377c50b26
MDN <mark.denardo@gmail.com>
2022-08-07 01:22:35 -0400

    Update color.js

--- color.js
+++ color.js
@@ -1,6 +1,11 @@
 //colors change based on audio source
 //MDN
+//const f=(a.fft[0])
+//osc(f,1,1).colorama(f,0.1).modulateScrollX(osc(f,1,1)).out(o0)
+//shape(f).modulateScrollX(f,1).out(o1)
+//render(o0)
 const f=(a.fft[0])
-osc(f,1,1).colorama(f,0.1).modulateScrollX(osc(f,1,1)).out(o0)
+osc(f,1,1).colorama(f,0.1).modulateScrollX(osc(f,1,1)).out()
+render()
 shape(f).modulateScrollX(f,1).out(o1)
 render(o0)
